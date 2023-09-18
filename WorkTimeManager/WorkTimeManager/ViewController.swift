4418db24a055686f2c223c51ad74f57dcd677e84
Denis Kutsko <denkua@gmail.com>
2023-09-18 11:13:14 +0300

    Create and configure project

---
+++ WorkTimeManager/WorkTimeManager/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WorkTimeManager
+//
+//  Created by Denis Kutsko on 18/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+}
+
